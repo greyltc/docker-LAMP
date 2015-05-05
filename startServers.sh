4c44e61027bf983f15264fdaf112f294088b262f
l3iggs <l3iggs@live.com>
2015-05-05 17:11:51 +0200

    fixup startServers

--- startServers.sh
+++ startServers.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+[ "$START_MYSQL" = true ] && cd /usr && /usr/bin/mysqld_safe --datadir=/var/lib/mysql&
 [ "$START_APACHE" = true ] && apachectl start
-[ "$START_MYSQL" = true ] && cd /usr && /usr/bin/mysqld_safe --datadir=/var/lib/mysql
+
+# hang out right here until the image is terminated
+sleep infinity 
