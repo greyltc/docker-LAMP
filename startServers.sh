020009c0d2d72234e6984ff65515c5db22cc1f57
l3iggs <john.f.grub@gmail.com>
2015-05-06 11:21:27 +0200

    add ability to re-generate ssl cert at every run

--- startServers.sh
+++ startServers.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+
 [ "$START_MYSQL" = true ] && cd /usr && /usr/bin/mysqld_safe --datadir=/var/lib/mysql&
+[ "$REGENERATE_SSL_CERT" = true ] && /etc/httpd/conf/genSSLKey.sh
 [ "$START_APACHE" = true ] && apachectl start
 
 # hang out right here until the image is terminated
-sleep infinity 
+sleep infinity
