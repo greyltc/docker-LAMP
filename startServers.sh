e4d9fb410aab5130bfb0171e770922e9bb6e3268
l3iggs <l3iggs@users.noreply.github.com>
2015-05-28 13:50:32 +0200

    add dav support to apache conf if it's turned on

--- startServers.sh
+++ startServers.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+if [ "$ENABLE_DAV" = true ] ; then
+  cat >> /etc/httpd/conf/httpd.conf <<EOF
+Alias /dav "/home/httpd/html/dav"
+
+<Directory "/home/httpd/html/dav">
+  DAV On
+  AllowOverride None
+  Options Indexes FollowSymLinks
+  Require all granted
+</Directory>
+EOF
+fi
 
 [ "$START_MYSQL" = true ] && cd /usr && /usr/bin/mysqld_safe --datadir=/var/lib/mysql&
 [ "$REGENERATE_SSL_CERT" = true ] && /etc/httpd/conf/genSSLKey.sh
