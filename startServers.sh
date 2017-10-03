7fab80229587e4dfa5c88050df331cc8aa3056d8
Grey Christoforo <grey@christoforo.net>
2017-10-03 15:08:01 +0200

    Update startServers.sh

--- startServers.sh
+++ startServers.sh
@@ -9,10 +9,10 @@ else
 fi
 
 # enable/disable non-https (unencrypted over port 80) apache access
-if [ "$APACHE_DISABLE_PORT_80" = true ] ; then
-  sed -i 's,^Listen 80,#Listen 80,g' /etc/httpd/conf/httpd.conf
-else
+if [ "$APACHE_ENABLE_PORT_80" = true ] ; then
   sed -i 's,^#Listen 80,Listen 80,g' /etc/httpd/conf/httpd.conf
+else
+  sed -i 's,^Listen 80,#Listen 80,g' /etc/httpd/conf/httpd.conf
 fi
 
 # the systemd services generally create these folders, make them now manually
